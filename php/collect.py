bdc2459cac21b26541a361844a9bcb08a666bf6c
Chris Timperley <christimperley@googlemail.com>
2016-06-01 11:17:50 +0100

    Added JSON document generation

--- php/collect.py
+++ php/collect.py
@@ -12,36 +12,26 @@ def process_bug(id):
     r = requests.get('https://bugs.php.net/bug.php', params={'id': id})
     doc = html.fromstring(r.content)
 
-    # Fetch meta-data and detailed report
-    summary         = read_field(doc, '//td[@id="summary"]')
-    status          = read_field(doc, '//tr[@id="categorization"]/td[1]')
-    package         = read_field(doc, '//tr[@id="categorization"]/td[2]/a')
-    submitted       = read_field(doc, '//tr[@id="submission"]/td[1]')
-    modified        = read_field(doc, '//tr[@id="submission"]/td[2]')
-    submitted_by    = read_field(doc, '//tr[@id="submitter"]/td[1]')
-    assigned_to     = read_field(doc, '//tr[@id="submitter"]/td[2]/a')
-    version         = read_field(doc, '//tr[@id="situation"]/td[1]')
-    os              = read_field(doc, '//tr[@id="situation"]/td[2]')
-    private         = read_field(doc, '//tr[@id="private"]/td[1]')
-    cve             = read_field(doc, '//tr[@id="private"]/td[2]')
-    note            = doc.xpath('//pre[@class="note"]/text()')[0]
+    # Collate meta-data and bug report into a JSON doc
+    jsn = {
+        'summary':      read_field(doc, '//td[@id="summary"]'),
+        'status':       read_field(doc, '//tr[@id="categorization"]/td[1]'),
+        'package':      read_field(doc, '//tr[@id="categorization"]/td[2]/a'),
+        'submitted':    read_field(doc, '//tr[@id="submission"]/td[1]'),
+        'modified':     read_field(doc, '//tr[@id="submission"]/td[2]'),
+        'submitter':    read_field(doc, '//tr[@id="submitter"]/td[1]'),
+        'assigned':     read_field(doc, '//tr[@id="submitter"]/td[2]/a'),
+        'version':      read_field(doc, '//tr[@id="situation"]/td[1]'),
+        'os':           read_field(doc, '//tr[@id="situation"]/td[2]'),
+        'private':      read_field(doc, '//tr[@id="private"]/td[1]'),
+        'cve':          read_field(doc, '//tr[@id="private"]/td[2]'),
+        'note':         doc.xpath('//pre[@class="note"]/text()')[0]
+    }
 
     # Find all SVN/Git commits
     commits = doc.xpath('//div[@class="comment type_svn"]/pre[1]/a/@href')
     commits = filter(lambda c: not c.startswith('bug.php'), commits)
     print commits
-
-#    print "ID: %d" % id
-#    print "Summary: %s" % summary
-#    print "Submitted: %s" % submitted
-#    print "Modified: %s" % modified
-#    print "Status: %s" % status
-#    print "Package: %s" % package
-#    print "Version: %s" % version
-#    print "OS: %s" % os
-#    print "Private: %s" % private
-#    print "CVE: %s" % cve
-#    print "\n\n%s" % note
 
 #
 process_bug(63740)
