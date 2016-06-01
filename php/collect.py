9c52056f08364fc072b53d916da670bedd00e1b4
Chris Timperley <christimperley@googlemail.com>
2016-06-01 12:24:26 +0100

    Added writing to disk to PHP collector

--- php/collect.py
+++ php/collect.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+import json
 import requests
+import os.path
 from lxml import html
 
 def read_field(doc, field):
@@ -7,13 +9,33 @@ def read_field(doc, field):
   res = doc.xpath(query)
   return (res[0] if len(res) == 1 else "")
 
-# Extracts information about a bug with a given ID from bugs.php.net
-def process_bug(id):
+# Extracts information about a bug with a given ID from bugs.php.net, returned
+# in the form of a dictionary object, ready for merging into a JSON document.
+# Returns None if there is no bug with the given id
+#
+# Note: id must be greater than zero
+def fetch_bug(id):
+    print "fetching bug: %d" % id
+    file_path = './bug-%d.json' % id
+
+    # Have we already downloaded this bug to disk?
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as f:
+            return json.load(f)
+
     r = requests.get('https://bugs.php.net/bug.php', params={'id': id})
     doc = html.fromstring(r.content)
 
-    # Collate meta-data and bug report into a JSON doc
+    # Does the bug exist?
+    if doc.xpath('//div[@class="errors"]/ul/li'):
+        return None
+
+    # Find all SVN/Git commits
+    commits = doc.xpath('//div[@class="comment type_svn"]/pre[1]/a/@href')
+    commits = filter(lambda c: not c.startswith('bug.php'), commits)
+
     jsn = {
+        'id':           id,
         'summary':      read_field(doc, '//td[@id="summary"]'),
         'status':       read_field(doc, '//tr[@id="categorization"]/td[1]'),
         'package':      read_field(doc, '//tr[@id="categorization"]/td[2]/a'),
@@ -25,13 +47,24 @@ def process_bug(id):
         'os':           read_field(doc, '//tr[@id="situation"]/td[2]'),
         'private':      read_field(doc, '//tr[@id="private"]/td[1]'),
         'cve':          read_field(doc, '//tr[@id="private"]/td[2]'),
+        'commits':      commits,
         'note':         doc.xpath('//pre[@class="note"]/text()')[0]
     }
 
-    # Find all SVN/Git commits
-    commits = doc.xpath('//div[@class="comment type_svn"]/pre[1]/a/@href')
-    commits = filter(lambda c: not c.startswith('bug.php'), commits)
-    print commits
+    # Save to disk
+    file_path = './bug-%d.json' % id
+    with open(file_path, 'w') as f:
+        json.dump(jsn, f, indent=2)
 
-#
-process_bug(63740)
+    return jsn
+
+# Single-threaded, for now
+def fetch_bugs():
+    doc = {} 
+    for id in range(1, 10):
+        doc[str(id)] = fetch_bug(id)
+    with open('bugs.json', 'w') as f:
+        json.dump(doc, f, indent=2)
+    return doc
+   
+fetch_bugs()
