5c08064100525b39b32ebccf344723895ce00fbf
Chris Timperley <christimperley@googlemail.com>
2016-06-02 22:03:38 +0100

    Tightened up PHP filter

--- php/filter.py
+++ php/filter.py
@@ -2,10 +2,19 @@
 import sys
 import json
 
+IGNORED_PACKAGES = ['Compile Failure', 'Feature/Change Request',
+    'Installation problem', 'Documentation problem']
+
 def keep_bug(bug):
-    return  bug['status'] == 'Closed' and \
-                len(bug['commits']) == 1 and \
-                (bug['commits'][0].startswith('http://git.php.net') or bug['commits'][0].startswith('http://svn.php.net'))
+    if bug['status'] != 'Closed':
+        return False
+    if len(bug['commits']) != 1:
+        return False
+    if not (bug['commits'][0].startswith('http://git.php.net') or bug['commits'][0].startswith('http://svn.php.net')):
+        return False
+    if bug['package'] in IGNORED_PACKAGES:
+        return False
+    return True
 
 def filter_bugs(in_file, out_file):
     with open(in_file, 'r') as f:
@@ -14,6 +23,7 @@ def filter_bugs(in_file, out_file):
     for (id, bug) in bugs.iteritems():
         if keep_bug(bug):
             filtered[id] = bug
+    print "filtered to %d bugs" % len(filtered)
     with open(out_file, 'w') as f:
         json.dump(filtered, f, indent=2, sort_keys=True)
 
