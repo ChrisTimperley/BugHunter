abe0472306e92bd75a72ac0737562500e2e328b7
Chris Timperley <christimperley@googlemail.com>
2016-08-04 18:33:10 +0100

    updated miner script

--- php2/miner.py
+++ php2/miner.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python
 import sys
 import git
+import json
 
 # Any commit containing a bug marker will be treated as a bug fix, unless the
 # commit also contains an anti-marker
 BUG_MARKERS = ['fixed', 'bug']
-BUG_ANTI_MARKERS = ['compile', 'compilation', 'debug', 'merge']
+BUG_ANTI_MARKERS = ['compile', 'compilation', 'debug', 'merge', 'revert']
 
 # Determines whether a given commit contains the fix to a bug
 def commit_contains_fix(commit):
     msg = commit.message.lower()
     return  any(s in msg for s in BUG_MARKERS) and \
                 not (any (s in msg for s in BUG_ANTI_MARKERS))
+
+def commit_to_json(commit):
+    return {
+        'id': str(commit),
+        'message': commit.message.strip(),
+        'committer': {
+            'email': commit.committer.email,
+            'name': commit.committer.name
+        },
+        'date': commit.committed_date
+    }
+
+def fixes_to_json_file(fixes, fn):
+    with open(fn, 'w') as f:
+        json.dump(map(commit_to_json, fixes), f, indent=2)
 
 # Supply the path to the repository via the command line
 repoPath = sys.argv[1]
@@ -22,4 +38,4 @@ repo = git.Repo(repoPath, odbt=git.GitCmdObjectDB)
 
 # Find the list of all suitable commits
 commits = filter(commit_contains_fix, repo.iter_commits())
-print len(commits)
+fixes_to_json_file(commits, "fixes.json")
