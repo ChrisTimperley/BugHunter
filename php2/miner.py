ecb9cf86ef1a48af491a034431679b77efd237ce
Chris Timperley <christimperley@googlemail.com>
2016-08-04 18:14:47 +0100

    new approach to bug mining!

---
+++ php2/miner.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import sys
+import git
+
+# Any commit containing a bug marker will be treated as a bug fix, unless the
+# commit also contains an anti-marker
+BUG_MARKERS = ['fixed', 'bug']
+BUG_ANTI_MARKERS = ['compile', 'compilation', 'debug', 'merge']
+
+# Determines whether a given commit contains the fix to a bug
+def commit_contains_fix(commit):
+    msg = commit.message.lower()
+    return  any(s in msg for s in BUG_MARKERS) and \
+                not (any (s in msg for s in BUG_ANTI_MARKERS))
+
+# Supply the path to the repository via the command line
+repoPath = sys.argv[1]
+
+# Model the repository using GitCmdObjectDB as it gives better performance
+# than GitDB when dealing with large quantities of smaller files.
+repo = git.Repo(repoPath, odbt=git.GitCmdObjectDB)
+
+# Find the list of all suitable commits
+commits = filter(commit_contains_fix, repo.iter_commits())
+print len(commits)
