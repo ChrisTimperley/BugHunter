27f3ffb8a8399ad26e8332a415ee4619e31602c6
Chris Timperley <christimperley@googlemail.com>
2016-08-04 20:18:42 +0100

    fixed lowercase checking in miner.py

--- php2/miner.py
+++ php2/miner.py
@@ -8,27 +8,61 @@ import json
 BUG_MARKERS = ['fixed', 'bug']
 BUG_ANTI_MARKERS = ['compile', 'compilation', 'debug', 'merge', 'revert']
 
+class Fix(object):
+    def __init__(self, commit):
+        self.__commit = commit
+        self.__stats = None
+        self.__files = None
+
+    def identifier(self):
+        return str(self.__commit)
+    def message(self):
+        return self.__commit.message.strip()
+    def email(self):
+        return self.__commit.committer.email
+    def name(self):
+        return self.__commit.committer.name
+    def date(self):
+        return self.__commit.committed_date
+
+    def stats(self):
+        if not self.__stats:
+            self.__stats = self.__commit.stats
+        return self.__stats
+    def files(self):
+        if not self.__files:
+            self.__files = self.stats().files.keys()
+        return self.__files
+
+    # Determines whether this object is indeed a fix
+    def is_fix(self):
+        msg = self.message().lower()
+        return  any(s in msg for s in BUG_MARKERS) and \
+                (not (any (s in msg for s in BUG_ANTI_MARKERS))) and \
+                any(f.endswith('.c') for f in self.files())
+
+    def to_json(self):
+        return {
+            'id': self.identifier(),
+            'message': self.message(),
+            'committer': {
+                'email': self.email(),
+                'name': self.name()
+            },
+            'date': self.date(),
+            'files': self.files()
+        }
+
 # Determines whether a given commit contains the fix to a bug
 def commit_contains_fix(commit):
     msg = commit.message.lower()
     return  any(s in msg for s in BUG_MARKERS) and \
-                not (any (s in msg for s in BUG_ANTI_MARKERS))
-
-def commit_to_json(commit):
-    return {
-        'id': str(commit),
-        'message': commit.message.strip(),
-        'committer': {
-            'email': commit.committer.email,
-            'name': commit.committer.name
-        },
-        'date': commit.committed_date,
-        'files': [commit.stats.files.keys()]
-    }
+                not (any (s in msg for s in BUG_ANTI_MARKERS)) and \
+                any(f.endswith('.c') for f in commit.stats.files.keys())
 
 def fixes_to_json_file(fixes, fn):
     with open(fn, 'w') as f:
-        json.dump(map(commit_to_json, fixes), f, indent=2)
+        json.dump(map(Fix.to_json, fixes), f, indent=2)
 
 # Supply the path to the repository via the command line
 repoPath = sys.argv[1]
@@ -38,5 +72,10 @@ repoPath = sys.argv[1]
 repo = git.Repo(repoPath, odbt=git.GitCmdObjectDB)
 
 # Find the list of all suitable commits
-commits = filter(commit_contains_fix, repo.iter_commits())
-fixes_to_json_file(commits, "fixes.json")
+print "extracting commits from repository..."
+fixes = map(Fix, repo.iter_commits())
+print "filtering fixes from list of commits..."
+fixes = filter(Fix.is_fix, fixes)
+print "found %d fixes" % len(fixes)
+print "writing fix information to JSON file..."
+fixes_to_json_file(fixes, "fixes.json")
