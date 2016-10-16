7d0ce68fa31b46dba977416094a9b6d40dffcec3
Chris Timperley <christimperley@googlemail.com>
2016-10-16 23:19:23 +0100

    added Storage class

---
+++ lib/storage.py
@@ -0,0 +1,21 @@
+# The Storage class is responsible for abstracting away the details of how and
+# where BugHunter's artefacts are stored, including pre-processed, parsed, and
+# differenced files.
+
+# - add zipped storage?
+class Storage(object):
+
+    def __init__(self, location):
+        pass
+
+    # copy?
+
+    # Returns the absolute path of a particular file within this file, given
+    # details of the fix it belongs to.
+    def file_path(self, repo, fix, version, fn):
+        pass
+
+    # Adds a source code file for a particular fix version into storage.
+    # Copies the file from the provided source into this storage.
+    def add_source_file_from(self, repo, fix, version, fn, src_fn):
+        pass
--- preprocessor.py
+++ preprocessor.py
@@ -1,30 +1,6 @@
 # This module is responsible for producing the pre-processed form of each file
 # modified by a given edit.
-#
-# - write pre-processed files to storage destination
-#
-
-# The Compiler class is responsible for
-
-
-# - add zipped storage?
-# - 
-class Storage(object):
-
-    def __init__(self, location):
-        pass
-
-    # copy?
-
-    # Returns the absolute path of a particular file within this file, given
-    # details of the fix it belongs to.
-    def file_path(self, repo, fix, version, fn):
-        pass
-
-    # Adds a source code file for a particular fix version into storage.
-    # Copies the file from the provided source into this storage.
-    def add_source_file_from(self, repo, fix, version, fn, src_fn):
-        pass
+import os
 
 # Compiles the source code for a project, together with its pre-processed
 # files.
@@ -34,9 +10,10 @@ class Storage(object):
 class Preprocessor(object):
     def __init__(self, threads = 8):
         self.__threads = threads
+        self.__script = os.path.dirname(os.path.realpath(__file__))
 
     def compile(self, repo, fix, version):
-        pass
+        cmd = "bash '%s'"
 
     def preprocess(self, repo, fix):
         self.compile(repo, fix, version)
