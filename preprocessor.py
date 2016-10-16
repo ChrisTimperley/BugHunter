b105e683da7edd62a390e8c04974eeba50fbca34
Chris Timperley <christimperley@googlemail.com>
2016-10-16 23:05:11 +0100

    more work on preprocessing and storage

--- preprocessor.py
+++ preprocessor.py
@@ -7,19 +7,37 @@
 # The Compiler class is responsible for
 
 
-
+# - add zipped storage?
+# - 
 class Storage(object):
 
     def __init__(self, location):
         pass
 
+    # copy?
+
     # Returns the absolute path of a particular file within this file, given
     # details of the fix it belongs to.
-    def file_path(self, repo, fix, fn, version):
+    def file_path(self, repo, fix, version, fn):
         pass
+
+    # Adds a source code file for a particular fix version into storage.
+    # Copies the file from the provided source into this storage.
+    def add_source_file_from(self, repo, fix, version, fn, src_fn):
+        pass
+
+# - compile via docker
+def preprocess(repo, fix, version):
+    self.compile(repo, fix, version)
+    for fn in fix.modified_source_files():
+        repo_fn = copy_source_file_to(repo.file_path(fn))
+        storage.add_source_file_from(repo, fix, version, fn, repo_fn)
 
 # Compiles the source code for a project, together with its pre-processed
 # files.
+#
+# We need to run a whole bunch of things on our Docker instance, then save
+# it using the Storage handler.
 class Compiler(object):
     def __init__(self, threads):
         self.threads = threads
