dd336d41f5b20bb1b56805aa09fc799629285803
Chris Timperley <christimperley@googlemail.com>
2016-10-17 11:10:28 +0100

    Updated Storage interface

--- src/storage.py
+++ src/storage.py
@@ -1,28 +1,19 @@
 # The Storage class is responsible for abstracting away the details of how and
 # where BugHunter's artefacts are stored, including pre-processed, parsed, and
 # differenced files.
-
-# - add zipped storage?
 class Storage(object):
 
-    def __init__(self, location):
-        pass
+    # Returns a handler for a given (pre-processed) source code file
+    def source(self, repo, fix, ver, fn):
+        raise NotImplementedError("No 'source' implemented by this Storage class")
 
-    # copy?
-
-    # Returns the absolute path of a particular file within this file, given
-    # details of the fix it belongs to.
-    def file_path(self, repo, fix, version, fn):
-        pass
-
-    # Adds a source code file for a particular fix version into storage.
-    # Copies the file from the provided source into this storage.
-    def add_source_file_from(self, repo, fix, version, fn, src_fn):
-        pass
-
+    # Returns a handler for a given diff
     def diff(self, repo, fix, fn):
-        # Do we have the 
-        return ZippedDiffFile(repo, fix, fn)
+        raise NotImplementedError("No 'diff' implemented by this Storage class")
+
+    # Returns a handler for the AST of a given (pre-processed) file
+    def ast(self, repo, fix, ver, fn):
+        raise NotImplementedError("No 'ast' implemented by this Storage class")
 
 class FlatStorage(object):
     pass
