a8d95a18f50b807b51cd0094c77b9fb72e6e4318
Chris Timperley <christimperley@googlemail.com>
2016-10-17 21:43:24 +0100

    more work on preprocessor

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -15,8 +15,29 @@ class Preprocessor(object):
     def compile(self, repo, fix, version):
         cmd = "bash '%s'"
 
-    def preprocess(self, repo, fix):
-        self.compile(repo, fix, version)
+    def preprocess(self, version):
+        repo = version.repository().repository()
+
+        # Find the commit ID for this program version
+        fix = version.fix()
+        commit = version.identifier()
+        if version.is_faulty():
+            commit = "%s~1" % commit
+
+        # Switch the repository's preprocessing branch to point at the
+        # correct commit for this program version
+        current_branch_name = repo.active_branch_name
+        try:         
+            repo.git.reset('--hard')
+            repo.git.checkout(commit, b='preprocessing')
+
+        finally:
+            repo.git.clean('-f', '-x')
+            repo.git.reset('--hard')
+            repo.git.checkout(current_branch_name)
+            repo.git.branch('-D', 'preprocessing')
+
+
         for fn in fix.modified_source_files():
             # shutil.copyfile(cp_from, cp_to)
             storage.add_source_file_from(repo, fix, version, fn, repo.file_path(fn))
