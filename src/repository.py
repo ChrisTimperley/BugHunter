93987bf82165f914eac8d8e93256a65d6d40fad4
Chris Timperley <christimperley@googlemail.com>
2016-10-17 19:38:04 +0100

    tidied up repository

--- src/repository.py
+++ src/repository.py
@@ -1,53 +1,20 @@
-import json
-import git
+import hashlib as hsh
 
+# Used to provide access to a Git repository and its mined edits
 class Repository(object):
 
     def __init__(self, storage, url):
         self.__storage = storage
         self.__url = url
-        self.__location = location
+        
+        # Storage handler is reponsible for retrieving and storing repos
+        #self.__repo = git.Repo(repoPath, odbt=git.GitCmdObjectDB)
 
-    # Returns an 
+    # Returns a short, file-name friendly identifier for this repository.
+    def id(self):
+        return hsh.sha1(self.__url)
 
-    # Finds all bug fixes within this repository, then saves their details to a
-    # JSON database file
-    def find_fixes(self):
-        pass
-
-    # Compute list of fixes if necessary
-    def fixes(self):
-        indexFile = self.__storage.fixIndex(self)
-
-        # generate fixes
-        if not indexFile.exists():
-            fixes = self.__scanner.scan(self)
-            indexFile.write(fixes)
-            
-        # load from file
-        else:
-            indexFile = indexFile.as_readable_file()
-            fixes = indexFile.read()
-            fixes = json.load(indexFile)
-            fixes = [Fix.from_json(fx) for fx in fixes]
-            indexFile.close()
-
-        return fixes
-
-class DatabaseFile(object):
-
-    # Returns a list of the bug fixes contained within this database file
-    def read(self):
-        jsn = json.load(storage.reader(self))
-        return [Fix.from_json(fx) for fx in fixes]
-
-    # Writes a list of bug fixes to this database file
-    def write(self, fixes):
-        f = storage.writer(self)
-        json.dump([Fix.to_json(fx) for fx in self.__fixes], f, indent=2)
-
-
-# Example
-repo = BugHunter.repository("https://github.com/php/php-src")
-for fix in repo.fixes():
-    print(fix.before().preprocessed("abc.c"))
+    # Returns a list of all bug fixes for this repository, using a provided
+    # scanner to generate them (if they aren't already saved to disk).
+    def fixes(self, scanner):
+        return self.__storage.databaseFile(self).read(scanner)
