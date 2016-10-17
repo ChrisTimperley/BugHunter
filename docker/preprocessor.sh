0da56cf25fa32681c47038eb490d00e855ce6d89
Chris Timperley <christimperley@googlemail.com>
2016-10-17 22:29:33 +0100

    working on preprocessor script

--- docker/preprocessor.sh
+++ docker/preprocessor.sh
@@ -4,25 +4,29 @@
 # directory. This script is intended to be executed on a Docker instance, and
 # not on the host machine (lib/preprocess should be used for that purpose).
 #
-fail_msg(){
+fail(){
   echo "FAILED: $1"
+  exit 1
 }
 
+# Fetch the provided user ID and the number of available threads
+userid=$1
+threads=$2
+
 # Jump to the correct location
-src_dir=$1
-cd $src_dir || fail_msg "failed to jump to source directory" && exit 1
+cd /repository || fail "failed to jump to source directory"
 
 # Generate configure, if necessary
-test -f autogen.sh && ./autogen.sh || fail_msg "autogen" && exit 1
-test -f buildconf.sh && ./buildconf.sh || fail_msg "buildconf" && exit 1
-test -f Makefile.am && autoreconf -i || fail_msg "autoreconf" && exit 1
+test -f autogen.sh && ./autogen.sh || fail "autogen"
+test -f buildconf.sh && ./buildconf.sh || fail "buildconf"
+test -f Makefile.am && autoreconf -i || fail "autoreconf"
 
 # Avoids Redis bugs
-test -f Makefile && make distclean || fail_msg "make distclean" && exit 1
+test -f Makefile && make distclean || fail "make distclean"
 
 # Attempt to configure
 if [ -f configure ]; then
-  ./configure "CFLAGS=-save-temps" || fail_msg "configure" && exit 1
+  ./configure "CFLAGS=-save-temps" || fail "configure"
   configured=0
 else
   configure=1
@@ -30,7 +34,7 @@ fi
 
 # Attempt to make
 if [ ! -f Makefile ]; then
-  fail_msg "Makefile missing" && exit 1
+  fail "Makefile missing" && exit 1
 fi
 
 if $configured; then
