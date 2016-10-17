4d51d7b94681bba7ae161d23f98a176976e813e5
Chris Timperley <christimperley@googlemail.com>
2016-10-17 21:56:07 +0100

    more work on preprocessor

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,6 +1,8 @@
 # This module is responsible for producing the pre-processed form of each file
 # modified by a given edit.
 import os
+import subprocess
+import utility as util
 
 # Compiles the source code for a project, together with its pre-processed
 # files.
@@ -12,9 +14,6 @@ class Preprocessor(object):
         self.__threads = threads
         self.__script = os.path.dirname(os.path.realpath(__file__))
 
-    def compile(self, repo, fix, version):
-        cmd = "bash '%s'"
-
     def preprocess(self, version):
         repo = version.repository().repository()
 
@@ -25,19 +24,28 @@ class Preprocessor(object):
             commit = "%s~1" % commit
 
         # Switch the repository's preprocessing branch to point at the
-        # correct commit for this program version
+        # correct commit for this program version.
         current_branch_name = repo.active_branch_name
         try:         
             repo.git.reset('--hard')
             repo.git.checkout(commit, b='preprocessing')
 
+            # Compute and execute the pre-process command
+            cmd = os.path.join(os.path.dirname(os.path.realpath(__file__)),\
+                               "../lib/preprocess")
+            cmd  = "%s '%s'" % repo.working_dir
+            with open(os.devnull, "w") as f:
+                subprocess.call(cmd, stdout=f)
+
+        # Ensure the repository is returned to its original state, prior to
+        # pre-processing
         finally:
             repo.git.clean('-f', '-x')
             repo.git.reset('--hard')
             repo.git.checkout(current_branch_name)
             repo.git.branch('-D', 'preprocessing')
 
-
+        # Save each of the modified source files to storage
         for fn in fix.modified_source_files():
+            pass
             # shutil.copyfile(cp_from, cp_to)
-            storage.add_source_file_from(repo, fix, version, fn, repo.file_path(fn))
