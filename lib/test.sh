9441d7bcde6242a8f0f99a57123bcfcbc58675cd
Chris Timperley <christimperley@googlemail.com>
2016-11-16 22:43:39 -0500

    removed docker test

--- lib/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-HERE=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-REPO="/home/chris/bughunter/repositories/7ebd8d5f50cf871de31a96f81ebe8402c9fc2a06"
-DOCKER="bh"
-echo "hello!"
-$HERE/preprocess $REPO $DOCKER
