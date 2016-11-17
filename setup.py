c916ea92072b9767d7859f049c399ca0f731a2ce
Chris Timperley <christimperley@googlemail.com>
2016-11-16 22:01:27 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from setuptools import setup, find_packages
+
+setup(
+    name='bughunter',
+    version='0.0.1',
+    description='A Git repository mining tool for studying bugs in C programs',
+    long_description='TBA',
+    author='Chris Timperley',
+    author_email='christimperley@gmail.com',
+    url='https://github.com/ChrisTimperley/BugHunter',
+    license='MIT',
+    packages=find_packages()
+)
