9e7178cb455ea91f0ca4d9c7973845d4924d442d
Chris Timperley <christimperley@googlemail.com>
2017-01-08 13:22:58 -0500

    begun work on FileDiff class

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ MAINTAINER Chris Timperley "christimperley@gmail.com"
 ADD . /tmp/bughunter
 RUN apt-get update && \
     apt-get install -y python3-setuptools python3-pip && \
-    apt-get install -y jq vim && \
     pip3 install gitpython && \
     cd /tmp && \
     git clone https://github.com/ChrisTimperley/PythonCGum pycgum && \
@@ -15,7 +14,7 @@ RUN apt-get update && \
     apt-get remove -y python3-setuptools && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
-ADD example /example
-ADD pretty /usr/bin/pretty
+ADD test /test
+ADD docker/pretty /usr/bin/pretty
 RUN mkdir -p /bughunter
 ENV BUGHUNTER /bughunter
---
+++ bughunter/diff.py
@@ -0,0 +1,29 @@
+# Represents the difference between two files 
+class FileDiff(object):
+    def __init__(self, fix, name):
+        self.__fix = fix
+        self.__name = name
+    
+    # Returns the name of the file
+    def name(self):
+        return self.__name
+
+    # Returns the fix that this diff belongs to
+    def fix(self):
+        return self.__fix
+    
+    # Returns the 'before' state of the file
+    def before(self):
+        pass
+
+    # Returns the 'after' state of the file
+    def after(self):
+        pass
+
+    # Returns the PyCGum difference representation of this diff
+    def cgum(self):
+        pass
+
+    # Returns a list of repair actions mined from this diff
+    def actions(self):
+        pass
