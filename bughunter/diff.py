b926eff4fe3a49bd59ff5497309b563d0a2bdb31
Chris Timperley <christimperley@googlemail.com>
2017-01-08 13:58:47 -0500

    implemented before, after and cgum methods for FileDiff

--- bughunter/diff.py
+++ bughunter/diff.py
@@ -3,6 +3,7 @@ class FileDiff(object):
     def __init__(self, fix, name):
         self.__fix = fix
         self.__name = name
+        self.__cgum = None
     
     # Returns the name of the file
     def name(self):
@@ -14,16 +15,18 @@ class FileDiff(object):
     
     # Returns the 'before' state of the file
     def before(self):
-        pass
+        return self.cgum().before()
 
     # Returns the 'after' state of the file
     def after(self):
-        pass
+        return self.cgum().after()
 
     # Returns the PyCGum difference representation of this diff
     def cgum(self):
-        pass
+        if self.__cgum is None:
+            self.__cgum = self.__main.storage.diff(self)
+        return self.__cgum
 
     # Returns a list of repair actions mined from this diff
     def actions(self):
-        pass
+        raise NotImplementedError
