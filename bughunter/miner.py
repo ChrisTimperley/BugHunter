72aa3b5f47f164b66f170d48c03c5e1a8bc1faca
Chris Timperley <christimperley@googlemail.com>
2017-01-08 18:19:05 -0500

    serialised replace loop body

--- bughunter/miner.py
+++ bughunter/miner.py
@@ -602,20 +602,35 @@ class ReplaceLoopGuard(RepairAction):
        
 class ReplaceLoopBody(RepairAction):
     @staticmethod
+    def from_json(jsn, before, after):
+        before_loop = before.find(jsn['before_loop'])
+        after_loop = after.find(jsn['after_loop'])
+        assert not before_loop is None
+        assert not after_loop is None
+        return ReplaceLoopBody(before_loop, after_loop)
+
+    @staticmethod
     def detect(patch, stmts_bef, stmts_aft, actions):
         l = filter(lambda s: isinstance(s, cgum.statement.Loop), stmts_aft)
         l = map(lambda s: (patch.is_was(s), s), l)
         l = filter(star(lambda frm,to: not frm is None), l)
         l = filter(star(lambda frm,to: not frm.body().equivalent(to.body())), l)
         actions['ReplaceLoopBody'] =\
-            [ReplaceLoopBody(frm, to, frm.body(), to.body()) for (frm, to) in l]
+            [ReplaceLoopBody(frm, to) for (frm, to) in l]
 
-    def __init__(self, from_stmt, to_stmt, from_body, to_body):
+    def __init__(self, from_stmt, to_stmt):
         self.__from_stmt = from_stmt
         self.__to_stmt = to_stmt
-        self.__from_body = from_body
-        self.__to_body = to_body
 
+    def from_body(self):
+        return self.__from_stmt.body()
+    def to_body(self):
+        return self.__to_stmt.body()
+
+    def to_json(self):
+        return super().to_json({'before_loop': self.__from_stmt.number(), \
+                                'after_loop': self.__to_stmt.number()})
+ 
 ## ASSIGNMENT-RELATED OPERATORS
 class ModifyAssignment(RepairAction):
     @staticmethod
