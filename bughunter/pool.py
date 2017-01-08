c7391de6573d95ed91299471d7275baaf1e46555
Chris Timperley <christimperley@googlemail.com>
2017-01-07 20:29:33 -0500

    implemented and tested basic Statement donor pool

--- bughunter/pool.py
+++ bughunter/pool.py
@@ -1,16 +1,30 @@
-class DonorPool(object):
-    
-    def __init__(self, contents):
-        self.__contents = contents
+import cgum.statement
 
+class DonorPool(object):
+    def __init__(self, program, criterion):
+        nodes = program.collect(lambda n: criterion.include(n))
+        self.__contents = frozenset(hash(n) for n in nodes)
     def contents(self):
         return self.__contents
-    def add(self):
-        pass
     def contains(self, node):
         pass
 
-class UnlabelledDonorPool(object):
+# Used to specify which nodes should be entered into the donor pool
+class DonorPoolCriterion(object):
+    def include(self, node):
+        raise NotImplementedError
 
-    def contains(self, node):
-        pass
+# Donor pool containing all nodes from the corpus
+class AtomicCriterion(DonorPoolCriterion):
+    def include(self, node):
+        return True
+
+# Donor pool of statements
+class StatementCriterion(DonorPoolCriterion):
+    def include(self, node):
+        return isinstance(node, cgum.statement.Statement)
+
+# Donor pool of blocks
+class BlockCriterion(DonorPoolCriterion):
+    def include(self, node):
+        return isinstance(node, cgum.statement.Block)
--- example/pool.py
+++ example/pool.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import pprint
+from pprint import pprint
 import cgum.statement
 import cgum.program
+import bughunter.pool as pl
 
 prog = cgum.program.Program.from_source_file("replace-while-loop-body/after.c")
-stmts = prog.collect(lambda n: isinstance(n, cgum.statement.Statement))
+pool = pl.DonorPool(prog, pl.StatementCriterion())
 
-for stmt in stmts:
-    print("%s -> %s" % (stmt, hash(stmt)))
+pprint(pool.contents())
