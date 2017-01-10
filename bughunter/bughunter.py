d1d3119f24483d27014b5cf255a3825e0690f97c
Chris Timperley <christimperley@googlemail.com>
2017-01-10 13:52:48 -0500

    updated BugHunter master object

--- bughunter/bughunter.py
+++ bughunter/bughunter.py
@@ -13,13 +13,10 @@ class BugHunter(object):
     # We could have a shared pool of repository (and fix) instances, but
     # since we're writing to disk directly, and contents are lazily fetched,
     # there shouldn't be any synchronisation issues.
-    def repository(self, address, docker_image):
-        return repository.Repository(self, address, docker_image)
+    def repository(self, address):
+        return repository.Repository(self, address)
 
     def storage(self):
         return self.__storage
     def scanner(self):
         return self.__scanner
-# examples
-#
-# bh.repository("https://github.com/php/php-src", docker_file)
