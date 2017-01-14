62c95bbf7309f5d1b988d0c1be666a769af0c558
Chris Timperley <christimperley@googlemail.com>
2017-01-13 22:02:28 -0500

    added concrete and abstract donor pools

--- bughunter/pool.py
+++ bughunter/pool.py
@@ -5,8 +5,7 @@ import os.path
 
 class DonorPool(object):
     @staticmethod
-    def build(name, diff, criterion):
-        tree = diff.before().ast()
+    def build(name, tree, criterion):
         contents = tree.collect(lambda n: criterion.covers(n))
         contents = frozenset(hash(n) for n in contents)
         return DonorPool(name, contents)
@@ -51,20 +50,6 @@ class DonorPoolSet(object):
         'block': BlockCriterion()
     }
 
-    # Returns the location on disk of the donor pool file for
-    # a given diff
-    @staticmethod
-    def locator(diff):
-        storage = diff.fix().master().storage()
-        path = "%s.pool.json" % diff.clean_name()
-        path = os.path.join(diff.fix().repository().id(),\
-                            diff.fix().identifier(),\
-                            path)
-        path = os.path.join(storage.root(), "artefacts", path)
-        return path
-
-    # Saves a set of donor pools (given as a dictionary) to a specified
-    # cache file on disk, overwriting any existing contents
     @staticmethod
     def __save(pools, loc):
         pools = {n: pl.to_json() for (n, pl) in pools.items()}
@@ -72,13 +57,8 @@ class DonorPoolSet(object):
         with open(loc, "w") as f:
             json.dump(pools, f)
     
-    # Builds the set of donor pools for a given diff, using on-disk
-    # caches where possible
-    #
-    # TODO: check encoding
     @staticmethod
-    def build(diff):
-        loc = DonorPoolSet.locator(diff)
+    def build(diff, ast, loc):
         if os.path.exists(loc):
             with open(loc, "r") as f:
                 pools = json.load(f)
@@ -92,15 +72,15 @@ class DonorPoolSet(object):
         for (pname, criterion) in DonorPoolSet.POOLS.items():
             if pname in pools:
                 continue
-
             modified = True
-            pools[pname] = DonorPool.build(pname, diff, criterion)
+            pools[pname] = DonorPool.build(pname, ast, criterion)
 
         # update cache file, if necessary
+        # TODO: refactor
         if modified:
             DonorPoolSet.__save(pools, loc)
 
-        return DonorPoolSet(diff, pools)
+        return pools
 
     def __init__(self, diff, pools):
         self.__diff = diff
@@ -126,3 +106,40 @@ class DonorPoolSet(object):
             if pool.contains(node):
                 return True
         return False
+
+class AbstractDonorPoolSet(object):
+    @staticmethod
+    def locator(diff):
+        storage = diff.fix().master().storage()
+        path = "%s.abstract.pool.json" % diff.clean_name()
+        path = os.path.join(diff.fix().repository().id(),\
+                            diff.fix().identifier(),\
+                            path)
+        path = os.path.join(storage.root(), "artefacts", path)
+        return path
+
+    @staticmethod
+    def build(diff):
+        loc = AbstractDonorPoolSet.locator(diff)
+        # TODO: strip labels
+        ast = diff.before().ast()
+        pools = DonorPoolSet.build(diff, ast, loc)
+        return AbstractDonorPoolSet(diff, pools)
+
+class ConcreteDonorPoolSet(DonorPoolSet):
+    @staticmethod
+    def locator(diff):
+        storage = diff.fix().master().storage()
+        path = "%s.concrete.pool.json" % diff.clean_name()
+        path = os.path.join(diff.fix().repository().id(),\
+                            diff.fix().identifier(),\
+                            path)
+        path = os.path.join(storage.root(), "artefacts", path)
+        return path
+
+    @staticmethod
+    def build(diff):
+        loc = ConcreteDonorPoolSet.locator(diff)
+        ast = diff.before().ast()
+        pools = DonorPoolSet.build(diff, ast, loc)
+        return ConcreteDonorPoolSet(diff, pools)
