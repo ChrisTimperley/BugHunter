575e8b542c096f784bac1fe91d6b8bdb1377dade
Chris Timperley <christimperley@googlemail.com>
2017-01-16 20:46:20 -0500

    refactored switch actions

--- bughunter/action/switch.py
+++ bughunter/action/switch.py
@@ -1,14 +1,10 @@
 from bughunter.action.core import *
 from bughunter.action.basic import ModifyStatement
 
-class ReplaceSwitchExpression(RepairAction):
+class ReplaceSwitchExpression(ReplaceRepairAction):
     @staticmethod
     def from_json(jsn, before, after):
-        before_stmt = before.find(jsn['before_stmt'])
-        after_stmt = after.find(jsn['after_stmt'])
-        assert not before_stmt is None
-        assert not after_stmt is None
-        return ReplaceSwitchExpression(before_stmt, after_stmt)
+        return ReplaceRepairAction.from_json(ReplaceSwitchExpression, jsn, before, after)
 
     @staticmethod
     def detect(patch, stmts_bef, stmts_aft, actions):
@@ -19,15 +15,10 @@ class ReplaceSwitchExpression(RepairAction):
         actions['ReplaceSwitchExpression'] =\
             [ReplaceSwitchExpression(frm, to) for (frm, to) in l]
 
-    def __init__(self, from_stmt, to_stmt):
-        self.__from_stmt = from_stmt
-        self.__to_stmt = to_stmt
-
     def from_expr(self):
-        return self.__from_stmt.expr()
+        return self.frm().expr()
     def to_expr(self):
-        return self.__to_stmt.expr()
+        return self.to().expr()
 
-    def to_json(self):
-        return super().to_json({'before_stmt': self.__from_stmt.number(),
-                                'after_stmt': self.__to_stmt.number()})
+    def parts(self):
+        return [self.to_expr()]
